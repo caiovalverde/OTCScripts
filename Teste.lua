7079cb71f6e5dcb10a2d45ed271876a6b2b18997
Caio Valverde <30451749+caiovalverde@users.noreply.github.com>
2022-07-16 02:57:55 -0300

    Update Teste.lua

--- Teste.lua
+++ Teste.lua
@@ -1,3 +1,11 @@
+setDefaultTab("Teste")
 macro(250, "Dancinha", "Ctrl+D", function()
     turn(math.random(0, 3)) -- turn to a random direction.
  end)
+
+macro(100, "PRISON50%", function()   
+                        if g_game.isAttacking() and g_game.getAttackingCreature():isPlayer() and g_game.getAttackingCreature():getHealthPercent() < 50 
+            then
+                        say("Suiton:Prision ")
+            end
+            end)
